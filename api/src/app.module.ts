eb4b9ed73b458584192f172d10a17fa6d3591163
ephys <hello@guylian.me>
2021-10-21 16:38:20 +0200

    feat: hide internal errors

--- api/src/app.module.ts
+++ api/src/app.module.ts
@@ -1,10 +1,10 @@
-import { Module } from '@nestjs/common';
 import { join } from 'path';
-import { DatabaseModule } from './database/database.module';
-import { ModpackModule } from './modpack/modpack.module';
-import { GraphQLModule } from '@nestjs/graphql';
 import { BullModule } from '@nestjs/bull';
+import { Module } from '@nestjs/common';
+import { GraphQLModule } from '@nestjs/graphql';
+import { DatabaseModule } from './database/database.module';
 import { ModModule } from './mod/mod.module';
+import { ModpackModule } from './modpack/modpack.module';
 import { ProjectSearchModule } from './project-search/project-search.module';
 
 @Module({
@@ -16,6 +16,24 @@ import { ProjectSearchModule } from './project-search/project-search.module';
     GraphQLModule.forRoot({
       autoSchemaFile: join(process.cwd(), 'src/schema.gql'),
       sortSchema: true,
+      formatError: error => {
+        const code = error.extensions?.code || 'INTERNAL_SERVER_ERROR';
+
+        if (code === 'INTERNAL_SERVER_ERROR') {
+          console.error(error.originalError);
+
+          return {
+            message: 'Internal Server Error',
+            code,
+          };
+        }
+
+        return {
+          message: error.extensions?.exception?.response?.message || error.message,
+          code,
+          // name: error.extensions?.exception?.name || error.name,
+        };
+      },
     }),
     BullModule.forRoot({
       redis: {
